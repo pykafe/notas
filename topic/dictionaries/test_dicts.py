f6cfdb6f9e978b5fe31e9b9777e7d9bcd6cf7db6
PeteCoward <peter.coward@gmail.com>
2016-01-31 10:21:46 +0900

    more tests

--- topic/dictionaries/test_dicts.py
+++ topic/dictionaries/test_dicts.py
@@ -1,3 +1,10 @@
+######################################################
+# Dicts
+# https://docs.python.org/2/library/stdtypes.html#dict
+#######################################################
+import pytest
+
+
 def empty_dict():
     """ Return an empty dict """
     pass
@@ -53,3 +60,101 @@ def test_is_key_in_dict():
     assert is_key_in_dict("Michael", test_dict) is True
     assert is_key_in_dict("Barack", test_dict) is True
     assert is_key_in_dict("Anders", test_dict) is False
+
+
+def dictionary_length(dictionary):
+    """ Return the length of the dictionary, the number of key/value pairs """
+    pass
+
+
+def test_dictionary_length():
+    test_dict = {
+        "hello": "world",
+        "Michael": "Jackson",
+        "Barack": "Obama"
+        }
+    assert dictionary_length(test_dict) == 3
+
+
+def add_key_value(dictionary, key, value):
+    """ Add a key value pair to a dictionary """
+    pass
+
+
+def test_add_key_value():
+    test_dict = {
+        "hello": "world",
+        "Michael": "Jackson",
+        "Barack": "Obama"
+        }
+
+    add_key_value(test_dict, "Rui", "Araujo")
+    assert test_dict["Rui"] == "Araujo"
+    assert test_dict["Michael"] == "Jackson"
+    assert len(test_dict) == 4
+
+
+def get_dictionary_keys(dictionary):
+    """ should return a list of all keys in a dictionary """
+    pass
+
+
+def test_get_dictionary_keys():
+    test_dict = {
+        "hello": "world",
+        "Michael": "Jackson",
+        "Barack": "Obama"
+        }
+
+    assert get_dictionary_keys(test_dict) == ['hello', 'Michael', 'Barack']
+
+
+def get_dictionary_values(dictionary):
+    """ should return a list of all values in a dictionary """
+    pass
+
+
+def test_get_dictionary_values():
+    test_dict = {
+        "hello": "world",
+        "Michael": "Jackson",
+        "Barack": "Obama"
+        }
+
+    assert get_dictionary_values(test_dict) == ['world', 'Jackson', 'Obama']
+
+
+def value_from_dictionary(dictionary, key):
+    """ Return the value from the dictionary stored under the key given """
+    pass
+
+
+def test_value_from_dictionary():
+    test_dict = {
+        "hello": "world",
+        "Michael": "Jackson",
+        "Barack": "Obama"
+        }
+
+    assert value_from_dictionary(test_dict, "Barack") == "Obama"
+
+    # check that an assertion is raised when we try to get a key that is not in the dictionary
+    with pytest.raises(KeyError):
+        value_from_dictionary(test_dict, "keynotpresent")
+
+
+def value_with_default(dictionary, key, default=None):
+    """ return the value from the dictionary stored under the key given, if it is not present return the default supplied """
+    pass
+
+
+def test_value_with_default():
+    test_dict = {
+        "hello": "world",
+        "Michael": "Jackson",
+        "Barack": "Obama"
+        }
+
+    assert value_with_default(test_dict, "Barack") == "Obama"
+    assert value_with_default(test_dict, "notpresent") is None
+    assert value_with_default(test_dict, "notpresent", "default") == "default"
