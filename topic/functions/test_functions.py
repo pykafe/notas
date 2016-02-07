e17897ae523d9898a75ec1f9ad225a7a996722f9
PeteCoward <peter.coward@gmail.com>
2016-02-07 12:11:29 +0900

    removed change apram test and added more checks

--- topic/functions/test_functions.py
+++ topic/functions/test_functions.py
@@ -11,6 +11,9 @@ my_simple_function = "this is not a function. Tenki troka"
 def test_my_function():
     # this line will raise an error if my_function is not callable
     my_simple_function()
+    # if your function accepts any variables, this test will fail
+    with pytest.raises(TypeError):
+        my_simple_function(1)
 
 
 # make my_returning_function a function that returns a value that does not evaluate to false
@@ -21,6 +24,9 @@ def test_my_function_returns_something():
     # this line will assert, fail the test, if my_returning_function returns a non true value
     returned_value = my_function_returns_something()
     assert returned_value
+    # if your function accepts any variables, this test will fail
+    with pytest.raises(TypeError):
+        my_function_returns_something(1)
 
 
 # make my_function_with_parameter a function that takes a parameter
@@ -30,6 +36,10 @@ my_function_with_parameter = "this is not a function. Tenki troka"
 def test_my_function_with_parameter():
     # this line will assert if your function does not accept a parameter
     my_function_with_parameter("a value")
+    # if your function accepts no variables, or more than one variable this test will fail
+    with pytest.raises(TypeError):
+        my_function_with_parameter()
+        my_function_with_parameter("a value", "another value")
 
 
 # make my_function_three_parameters a function that takes three parameters
@@ -39,6 +49,11 @@ my_function_three_parameters = "this is not a function. Tenki troka"
 def test_my_function_three_parameters():
     # this line will assert if your function does not accept a parameter
     my_function_three_parameters("a value", 23, ["this", "is", "a", "list"])
+    # if your function accepts anythin else expect 3 variables this test will fail
+    with pytest.raises(TypeError):
+        my_function_three_parameters()
+        my_function_three_parameters("a value", "another value")
+        my_function_three_parameters("a value", "another value", 2, 5)
 
 
 # make my_function_optional_parameter a function which can take a parameter, or not
@@ -50,6 +65,9 @@ def test_my_function_optional_parameter():
     my_function_optional_parameter()
     # this line checks we can call the function with a parameter
     my_function_optional_parameter("a value")
+    # if your function accepts more than 1 parameter this test will fail
+    with pytest.raises(TypeError):
+        my_function_optional_parameter(1, 2)
 
 
 # make a function with 1 required parameter and 1 optional parameter
@@ -61,8 +79,9 @@ def test_my_function_one_required_one_optional():
     my_function_one_required_one_optional("first")
     # this line will assert if your function does not accept 2 parameters
     my_function_one_required_one_optional("first", "second")
-    # this will fail if your function accepts more than 2 parameters
+    # this will fail if your function accepts no parameters, or more than 2 parameters
     with pytest.raises(TypeError):
+        my_function_one_required_one_optional()
         my_function_one_required_one_optional("first", "second", "third")
 
 
@@ -82,19 +101,9 @@ def test_my_function_two_named():
     my_function_two_named(param_a=1, param_b=2)
     # your function can decide which parameter is which based on the names given
     my_function_two_named(param_b=1, param_a=2)
-
-
-# make a function that changes the value of the parameter passed to it
-my_function_changes_parameter_value = "this is not a function. Tenki troka"
-
-
-def test_my_function_changes_parameter_value():
-    # set a variable to a value
-    my_variable = 5
-    # pass that variable into our function
-    my_function_changes_parameter_value(my_variable)
-    # fail the test if the value of the variable has not changed
-    assert my_variable != 5
+    # this will fail if your function accepts more than 2 parameters
+    with pytest.raises(TypeError):
+        my_function_two_named(1, 2, param_b=1, param_a=2)
 
 
 # This function accepts any number of unnamed arguments
