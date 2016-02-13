7c43cba05acf4e2a966247cfce579450ccb053aa
PeteCoward <peter.coward@gmail.com>
2016-02-13 10:54:46 +0900

    adding revision and iteration untested

---
+++ topic/iteration/test_iteration.py
@@ -0,0 +1,63 @@
+########################
+# Iteration
+########################
+
+
+def sum_of_integers(maximum):
+    ''' return the sum of all the integers up to maximum
+    hint: create a sum variable value 0, and a counter variable value 1
+    WHILE the counter is less than OR EQUAL TO the maximum, add the counter to the sum, add 1 to the counter
+    after the while loop has exited, return the sum
+    '''
+    pass
+
+
+def test_sum_of_integers():
+    assert sum_of_integers(2) == 3
+    assert sum_of_integers(5) == 15
+
+
+def ten_multiples_of(number):
+    ''' return a list containing 10 mutpliples of the input parameter number
+    hint: create an empty list, and a counter variable value 1
+    WHILE the lenght of the list is less than 10, append counter * number to the list, add 1 to the counter
+    after the while loop has exited, return the list
+    '''
+    pass
+
+
+def test_ten_multiples_of():
+    result_1 = ten_multiples_of(2)
+    assert isinstance(result_1, list)
+    assert len(result_1) == 10
+    assert sum([x % 2 for x in result_1])
+
+    result_2 = ten_multiples_of(5)
+    assert isinstance(result_2, list)
+    assert len(result_2) == 10
+    assert sum([x % 5 for x in result_2])
+
+
+def mulitply_odd_numbers_until(maximum):
+    ''' return the value of multiplying together all the odd integers up to maximum
+    hint: NO HINTS THIS TIME
+    '''
+
+
+def test_mulitply_odd_numbers_until():
+    assert mulitply_odd_numbers_until(5) == 1 * 3 * 5
+    assert mulitply_odd_numbers_until(9) == 1 * 3 * 5 * 7 * 9
+
+
+def sum_and_product(in_list):
+    ''' return 2 values from this function - a tuple
+    the first value is the sum of all the values in the list
+    the second alue is the product of all the values in the list
+    sum_and_product([2, 3]) == (6, 5)
+    sum_and_product([2, 4, 6]) == (12, 48)
+    '''
+
+
+def test_sum_and_product():
+    assert sum_and_product([2, 3]) == (6, 5)
+    assert sum_and_product([2, 4, 6]) == (12, 48)
---
+++ topic/revision/test_revision.py
@@ -0,0 +1,63 @@
+#########################
+# Revision
+#########################
+
+
+# this variable should be a string
+a_string = None
+# this variable should be an integer
+a_integer = None
+# this variable should be a float
+a_float = None
+# this variable should be a list
+a_list = None
+# this variable should be a dictionary
+a_dictionary = None
+
+
+def test_types():
+    assert isinstance(a_string, str)
+    assert isinstance(a_integer, int)
+    assert isinstance(a_float, float)
+    assert isinstance(a_list, list)
+    assert isinstance(a_dictionary, dict)
+
+
+def list_item(list_in, index):
+    # return the item in the list at the given index
+    pass
+
+
+def test_list_item():
+    test_list = [2, "two", "rua", "dua"]
+    assert list_item(test_list, 0) == 2
+    assert list_item(test_list, 2) == "rua"
+
+
+def dictionary_describe(dictionary_in):
+    ''' return a string describing the passed in dictionary in the following format
+    {0: 1, 3: 4, 8: 9} => "3 key value pairs, the first key is 0"
+    {"English": "Hello", "Tetum": "Bondia"} => "2 key value pairs, the first key is English"
+    '''
+    pass
+
+
+def test_dictionary_describe():
+    assert dictionary_describe({0: 1, 3: 4, 8: 9}) == "3 key value pairs, the first key is 0"
+    assert dictionary_describe({"English": "Hello", "Tetum": "Bondia"}) == "2 key value pairs, the first key is English"
+
+
+# This should be a function
+# it should take 2 parameters x, and y
+# it should return a list containing
+#  1. the two parameters added together
+#  2. the two parameters multiplied together
+#  2. the string "my first parameter was x, my second parameter was y"
+my_function = "this is a string, not a function. please change it"
+
+
+def test_my_function():
+    return_1 = my_function(1, 2)
+    assert 3 in return_1
+    assert 2 in return_1
+    assert "my first parameter was 1, my second parameter was 2" in return_1
